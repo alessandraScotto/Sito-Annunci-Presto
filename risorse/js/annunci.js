754f5e771bac9ae7b619a96124d577eb9a6215a6
IdaAlessandra ScottodiLuzio <idaalessandra.scottodiluzio@gmail.com>
2023-04-25 13:35:55 +0200

    header articles

--- risorse/js/annunci.js
+++ risorse/js/annunci.js
@@ -20,4 +20,36 @@ offcanvas.addEventListener('click', () => {
     openNav.innerHTML = '<i class="fs-2 fa-solid fa-bars-staggered"></i>';
 })
 
+
+
+
+
+
+
 //Sviluppo cards articoli
+fetch('../annunci.json')
+    .then(response => response.json())
+    .then(data => {
+        //Data = array di annunci
+
+        //Funzione per estrapolare le categorie
+        function setCategory() {
+            let uniqueCategories = [];
+            data.forEach(element => {
+             if (!uniqueCategories.includes(element.category)) {
+                uniqueCategories.push(element.category)
+             }
+            });
+        }
+        setCategory();
+
+
+
+
+
+
+
+
+
+    })
+
